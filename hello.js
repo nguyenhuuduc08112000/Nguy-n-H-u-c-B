7fb9f957ea9490cbfac9f0f53cde3b781ee9356a
nguyenhuuduc08112000 <huuducnguyen16@gmail.com>
2019-11-25 22:42:24 +0700

    .

---
+++ hello.js
@@ -0,0 +1,38 @@
+console.log('hello world');
+console.log("I'm Nguyen Huu Duc");
+console.log('I study at HaNoi Open University');
+console.log('i am 19');
+
+let name = "Duc";
+let age = "19";
+
+console.log("i'm " + name );
+console.log(`i'm ${age} years old`);
+
+let yob = prompt ('bo may bao nhieu tuoi : ');
+
+console.log(2019 + Number(yob));
+
+// clear()
+// for( let i = 0; i < 100 ; i++){
+//   fd(100)
+//   lt(90)
+//   fd(100)
+//   lt(90)
+//   fd(100)
+//   lt(90)
+//   fd(100)
+//   lt(90)
+//   lt(20)
+// }
+
+// clear()
+// color("red")
+// setshape("car")
+// for (let i = 0 ; i<6 ; i++){
+// for ( let i = 0; i<360 ; i++ ){
+//   fd(1)
+//   lt(1)
+//   }
+// lt(60)
+// }
